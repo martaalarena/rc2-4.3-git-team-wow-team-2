6e4820279ef489525d143802877f9efe7583498d
laraturrion <laturrions@gmail.com>
2024-02-06 10:07:29 +0100

    createtoy

--- src/main/java/net/toyland/store/controllers/toys/ToyController.java
+++ src/main/java/net/toyland/store/controllers/toys/ToyController.java
@@ -1,11 +1,22 @@
 package net.toyland.store.controllers.toys;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/v1/toys")
 public class ToyController {
+
     // COMPLETE THIS
+}
+
+
+@PostMapping
+public ResponseEntity<ToyResponse> createToy(@RequestBody ToyRequest toyRequest) {
+    Toy toy = new Toy(toyRequest.getName(), toyRequest.getBrand(), toyRequest.getPrice());
+    toyRepository.save(toy);
+    ToyResponse toyResponse = mapToToyResponse(toy);
+    return ResponseEntity.status(HttpStatus.CREATED).body(toyResponse);
 }
