5e736a42cade1fa59144c92ed0ce3c49fdfc8cd9
Iciar Rodríguez <iciar@MacBook-Pro-de-Iciar.local>
2024-02-06 10:12:31 +0100

    Funcion Delete Toy creada por iciar

--- src/main/java/net/toyland/store/controllers/toys/ToyController.java
+++ src/main/java/net/toyland/store/controllers/toys/ToyController.java
@@ -2,10 +2,35 @@ package net.toyland.store.controllers.toys;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import net.toyland.store.persistence.toys.Toy;
+import net.toyland.store.persistence.toys.ToyRepository;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/v1/toys")
 public class ToyController {
-    // COMPLETE THIS
+
+    private final ToyRepository toyRepository;
+
+    public ToyController(ToyRepository toyRepository) {
+        this.toyRepository = toyRepository;
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteToy(@PathVariable Long id) {
+        Optional<Toy> toyOptional = toyRepository.findById(id);
+        if (toyOptional.isPresent()) {
+            toyRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
